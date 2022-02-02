4d03cd880079d834cf897727e37e0a3b27cd14c4
Keegan <b.keegan12@yahoo.com>
2022-02-02 11:33:06 -0500

    Initial Commit

---
+++ src/main/java/me/koogen/coffeeplugin/CoffeePlugin.java
@@ -0,0 +1,17 @@
+package me.koogen.coffeeplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoffeePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
